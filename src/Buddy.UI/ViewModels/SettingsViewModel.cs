700b802c4c63469927f1193e462b7c52d1159419
aevitas <m4rtijn@gmail.com>
2016-01-27 12:35:57 +0100

    Add support for theme and accent selection

--- src/Buddy.UI/ViewModels/SettingsViewModel.cs
+++ src/Buddy.UI/ViewModels/SettingsViewModel.cs
@@ -1,8 +1,57 @@
-﻿using Buddy.UI.Core;
+﻿using System.Collections.ObjectModel;
+using Buddy.UI.Core;
+using Buddy.UI.Core.Interfaces;
+using Buddy.UI.Core.Themes;
 
 namespace Buddy.UI.ViewModels
 {
 	public class SettingsViewModel : ViewModel
 	{
+		private static SettingsViewModel _instance;
+
+		public static SettingsViewModel Instance => _instance ?? (_instance = new SettingsViewModel());
+
+		private SettingsViewModel()
+		{
+			
+		}
+
+		public ObservableCollection<ITheme> Themes => ThemeManager.Themes;
+
+		public ObservableCollection<IAccentColor> Accents => ThemeManager.Accents; 
+
+		private int _selectedThemeIndex;
+
+		public int SelectedThemeIndex
+		{
+			get { return _selectedThemeIndex; }
+			set
+			{
+				if (value < 0)
+					return;
+
+				SetProperty(ref _selectedThemeIndex, value);
+
+				var t = ThemeManager.Themes[value];
+				ThemeManager.LoadResources(t.Name, t.ResourceDictionary);
+			}
+		}
+
+		private int _selectedAccentIndex;
+
+		public int SelectedAccentIndex
+		{
+			get { return _selectedAccentIndex;}
+			set
+			{
+				if (value < 0)
+					return;
+
+				SetProperty(ref _selectedAccentIndex, value);
+
+				var a = ThemeManager.Accents[value];
+				ThemeManager.LoadResources(a.Name, a.ResourceDictionary);
+			}
+		}
 	}
 }
